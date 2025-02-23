f8d49629b5165aa9eb4fc2a4226e2fa08aa06c87
Ayoub-Mekhfioui <ayoubmekhfioui122@gmail.com>
2025-02-23 23:57:01 +0100

    Initial commit - Ajout du projet ThreadsTalkatives

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            Talkative talkative = new Talkative(i);
+            Thread thread = new Thread(talkative);
+            thread.start();
+        }
+    }
+}
